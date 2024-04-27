b9f2e2213e3306159209ab7f9416d16c95ab8369
emonkhan500 <khdilo47@gmail.com>
2024-04-28 02:04:59 +0600

    banner complete

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,51 @@
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination,Autoplay,Navigation } from 'swiper/modules';
+import { Link } from 'react-router-dom';
+
+const Banner = () => {
+    return (
+        <div className='container mx-auto mt-12'>
+         <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        loop={true}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        
+        pagination={{
+            clickable: true,
+        }} 
+        modules={[Pagination,Autoplay,Navigation]} className="mySwiper">
+        <SwiperSlide><div className='slide slide1'>
+           <div className='text-center'>
+           <h1 className='text-white font-bold text-5xl '>Experience <span className='text-red-600'>Asia</span> Tour Like Never Before<br /> Dive into Our Listings</h1>
+           <Link to="" className="btn btn-outline  bg-[#0BE58A] w-[180px] mt-5 text-[#150B2B] text-xl font-semibold rounded-xl">Explore Now</Link>
+           </div>
+           <br />
+           
+        </div></SwiperSlide>
+        <SwiperSlide><div className='slide slide2'> <div className='text-center'>
+           <h1 className='text-white font-bold text-5xl'>Experience <span className='text-red-600'>Asia</span> Tour Like Never Before <br /> Dive into Our Listings</h1>
+           <button className="btn btn-success bg-[#0BE58A] w-[180px] mt-5 text-[#150B2B] text-xl font-semibold rounded-xl">Explore Now</button>
+           </div>
+           <br /></div></SwiperSlide>
+        <SwiperSlide><div className='slide slide3'> <div className='text-center'>
+           <h1 className='text-white font-bold text-5xl'> Experience <span className='text-red-600'>Asia</span> Tour Like Never Before <br /> Dive into Our Listings</h1>
+           <button className="btn btn-success bg-[#0BE58A] w-[180px] mt-5 text-[#150B2B] text-xl font-semibold rounded-xl">Explore Now</button>
+           </div>
+           <br /></div></SwiperSlide>
+       
+      </Swiper>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
+
 
 const Home = () => {
     
     return (
-        <div>
-            <h1>this is home</h1>
+        <div className="mt-10 container mx-auto">
+           <Banner></Banner>
         </div>
     );
 };
