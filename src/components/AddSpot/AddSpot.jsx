c305e98659654af01b807cfb3270c15ce2f20b4e
emonkhan500 <khdilo47@gmail.com>
2024-04-28 21:25:38 +0600

    add page ui complete

--- src/components/AddSpot/AddSpot.jsx
+++ src/components/AddSpot/AddSpot.jsx
@@ -4,10 +4,82 @@ const AddSpot = () => {
     useEffect(()=>{
         document.title='Add Tourist Spot'
     },[])
+
+
+
+    const handleAddSpot = (e) => {
+        e.preventDefault();
+        const image = e.target.image.value;
+        const spot = e.target.spot.value;
+        const country = e.target.country.value;
+        const location = e.target.location.value;
+        const description = e.target.description.value;
+        const cost = e.target.cost.value;
+        const season = e.target.season.value;
+        const travel = e.target.travel.value;
+        const visitor = e.target.visitor.value;
+        const email= e.target.email.value;
+        const name = e.target.name.value;
+        const place = { image, spot,country,location, description,cost, season, travel, visitor ,email,name};
+        console.log(place);
+    
+        
+        
+      };
+
+
     return (
-        <div>
-            <h1>this is Add spot</h1>
+        <div className="bg-gray-100 p-6 mt-11 md:p-12">
+      <h2 className="text-3xl font-extrabold text-center mb-8 text-cyan-300">Add a Tourist Spot</h2>
+      <form onSubmit={handleAddSpot}>
+        <div className="mb-8">
+          <label className="block mb-2">Image URL</label>
+          <input type="text" name="image" placeholder="Image URL" className="input input-bordered w-full" />
         </div>
+        <div className="mb-8">
+          <label className="block mb-2">Tourists Spot Name</label>
+          <input type="text" name="spot" placeholder="tourists_spot_name" className="input input-bordered w-full" />
+        </div>
+        <div className="mb-8">
+          <label className="block mb-2">Country Name</label>
+          <input type="text" name="country" placeholder="country_Name" className="input input-bordered w-full" />
+        </div>
+        <div className="mb-8">
+          <label className="block mb-2">Location</label>
+          <input type="text" name="location" placeholder="location" className="input input-bordered w-full" />
+        </div>
+        <div className="mb-8">
+          <label className="block mb-2">Short Description</label>
+          <input type="text" name="description" placeholder="Short Description" className="input input-bordered w-full" />
+        </div>
+        <div className="mb-8">
+          <label className="block mb-2">Average Cost</label>
+          <input type="text" name="cost" placeholder="average_cost" className="input input-bordered w-full" />
+        </div>
+        <div className="mb-8">
+          <label className="block mb-2">Season</label>
+          <input type="text" name="season" placeholder="season" className="input input-bordered w-full" />
+        </div>
+        <div className="mb-8">
+          <label className="block mb-2">Travel Time</label>
+          <input type="text" name="travel" placeholder="travel_time" className="input input-bordered w-full" />
+        </div>
+        <div className="mb-8">
+          <label className="block mb-2">Total Visitow Per Year</label>
+          <input type="text" name="visitor" placeholder="totaVisitorsPerYear" className="input input-bordered w-full" />
+        </div>
+        <div><h2 className="text-3xl font-extrabold text-center mb-8 text-cyan-300">User Info</h2></div>
+        <div className="mb-8">
+          <label className="block mb-2">User Email</label>
+          <input type="text" name="email" placeholder="email" className="input input-bordered w-full" />
+        </div>
+        <div className="mb-8">
+          <label className="block mb-2">User Name</label>
+          <input type="text" name="name" placeholder="name" className="input input-bordered w-full" />
+        </div>
+        <button type="submit" className="btn btn-block btn-success">Add Spot</button>
+      </form>
+    </div>
     );
 };
 
